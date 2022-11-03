d6acee92075b11d901d1debd7a195985c743cc0a
LAPTOP-UEEQEUCE\Пользователь <agoronik@yandex.ru>
2022-11-04 02:58:00 +0300

    Added modal windoww, simple rest... prepare project to next task ... .

--- src/main/java/manager/mywebappspringboot/model/User.java
+++ src/main/java/manager/mywebappspringboot/model/User.java
@@ -30,7 +30,7 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
 
 
